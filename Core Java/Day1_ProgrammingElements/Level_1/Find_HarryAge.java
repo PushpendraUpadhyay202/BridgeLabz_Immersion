d240fb29fa0c2ccc45cc2f5e2935dfac3f9781bb
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2025-06-12 10:59:03 +0530

    Changing the name

--- Core Java/Day1_ProgrammingElements/Level_1/Find_HarryAge.java
+++ Core Java/Day1_ProgrammingElements/Level_1/Find_HarryAge.java
@@ -1,5 +1,5 @@
 // Write a program to find the age of Harry if the birth year is 2000. Assume the Current Year is 2024
-public class Lvl1_1 {
+public class Find_HarryAge {
     public static void main(String[] args) {
         int birthYear = 2000;
         int currentYear = 2024;
