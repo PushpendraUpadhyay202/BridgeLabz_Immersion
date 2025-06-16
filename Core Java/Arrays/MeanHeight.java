fe176dcea42e55c49d6c58f1673d9511b6168e34
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2025-06-16 09:31:32 +0530

    [Pushpendra Upadhyay] Add. program to create a 2D Array and copy the 2D Array into a single dimension array

--- Core Java/Arrays/MeanHeight.java
+++ Core Java/Arrays/MeanHeight.java
@@ -1,4 +1,4 @@
-//  Create a program to find the mean height of players in a football team. Hint => 1. The formula to calculate the mean is: mean = sum of all elements/number of elements 2. Create a double array named heights of size 11 and get input values from the user. 3. Find the sum of all the elements present in the array. 4. Divide the sum by 11 to find the mean height and print the mean height of the footballteam.
+//  Create a  of players inprogram to find the mean height a football team. Hint => 1. The formula to calculate the mean is: mean = sum of all elements/number of elements 2. Create a double array named heights of size 11 and get input values from the user. 3. Find the sum of all the elements present in the array. 4. Divide the sum by 11 to find the mean height and print the mean height of the footballteam.
 
 import java.util.Scanner;
 public class MeanHeight {
