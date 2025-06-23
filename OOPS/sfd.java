4bc345058a85ae12cc073db28b12730e67ba9d21
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2025-06-23 21:28:10 +0530

    [Pushpendra Upadhyay] Add . Creating a folder for OOPS Material

---
+++ OOPS/sfd.java
@@ -0,0 +1,7 @@
+package OOPS;
+
+public class sfd {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! This is a placeholder for the Java and Object class.");
+    }
+}
