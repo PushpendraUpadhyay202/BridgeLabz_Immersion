89c3ff1e173b55299faa7da9fa7c75225359a897
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2025-06-26 12:36:06 +0530

    fgvd

--- OOPS/sfd.java
+++
@@ -1,7 +0,0 @@
-package OOPS;
-
-public class sfd {
-    public static void main(String[] args) {
-        System.out.println("Hello, World! This is a placeholder for the Java and Object class.");
-    }
-}
