b93f9733b27f94e2a5d2650f2474f2a3936741a9
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2025-06-27 16:19:21 +0530

    Changing the name of parent folder

---
+++ OOPS/Part 1/Level1/Item.java
@@ -0,0 +1,25 @@
+package com.bridgelab_oops.level1;
+
+public class Item {
+
+	private int itemCode;
+	private String itemName;
+	private double price;
+	
+	public Item(int itemCode, String itemName, double price){
+		this.itemCode=itemCode;
+		this.itemName=itemName;
+		this.price=price;
+	}
+	
+	public void itemDetails() {
+		System.out.println("Item Details: ");
+		System.out.println("Item Code: " + itemCode);
+		System.out.println("Item Name: " + itemName);
+		System.out.println("Item Price: " + price);
+	}
+	
+	public void totalCost(int quantity) {
+		System.out.println("The total cost is: " + price * quantity);
+	}
+}
---
+++ OOPS/Part 1/Level1/MobilePhoneMain.java
@@ -0,0 +1,27 @@
+package com.bridgelab_oops.level1;
+
+import java.util.Scanner;
+
+public class MobilePhoneMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input from user
+        System.out.print("Enter Mobile Brand: ");
+        String brand = sc.nextLine();
+
+        System.out.print("Enter Mobile Model: ");
+        String model = sc.nextLine();
+
+        System.out.print("Enter Mobile Price: ");
+        double price = sc.nextDouble();
+
+        // Create MobilePhone object
+        MobilePhone phone = new MobilePhone(brand, model, price);
+
+        // Display the details
+        phone.displayDetails();
+
+        sc.close();
+    }
+}
